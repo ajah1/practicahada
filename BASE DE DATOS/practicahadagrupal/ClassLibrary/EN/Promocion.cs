55cc5aea58f9133f58aaf389d766045471be0179
aliagaHyder <ajah1@alu.ua.es>
2017-04-12 19:26:25 +0200

    Añadiendo atributos y metodos a ENPopular / ENPromocion
    Una vez que esté hecha la clase Producto falta quitar los comentarios y
    modificar lo que falte para la parte heredada

---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/EN/Promocion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.EN
+{
+    public class Promocion
+    {
+
+        private int id_promocion;
+        private double descuento;
+        private string f_inicio;
+        private string f_limite;
+        // private Producto producto;
+
+        public Promocion() { }
+
+        public 
+            Promocion(int id,
+                         double descuento,
+                         string f_inicio,
+                         string f_limite)
+                       //Producto producto
+        {
+            this.id_promocion = id;
+            this.descuento = descuento;
+            this.f_inicio = f_inicio;
+            this.f_limite = f_limite;
+            //this.producto = producto;
+        }
+
+        public int Id{ get; set;}
+        public double Descuento { get; set; }
+        public string Fecha_Inicio { get; set; }
+        public string F_limite { get; set; }
+
+        public 
+            void save(string dbname)
+        {
+            // esto depende de como funcione
+            // el gestor de la bd
+        }
+
+        public
+            void addPromocion(string dbname)
+        {
+
+        }
+
+        public
+            void removePromocion(string dbname)
+        {
+
+        }
+    }
+}
--- BASE DE DATOS/practicahadagrupal/ClassLibrary/EN/popular.cs
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/EN/popular.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary.EN
 {
-    class popular
+    public class Popular //:Producto
     {
+        // private int id; lo hereda de Producto
+        private int numero_ventas;
+        //private productos = new List<Producto> {};
+
+        public Popular() { }
+
+        public 
+            Popular(int numero_ventas)
+        {
+            this.numero_ventas = numero_ventas;
+        }
+        
+        public int Numero_ventas { get; set; }
+        //public List<Producto> Productos { get; }
+
+        public
+            void save (string dbname)
+        {
+            // esto depende de como funcione
+            // el gestor de la bd
+        }
+
+        public
+            void addPopular(string dbname)
+        {
+
+        }
+
+        public
+            void removePopular(string dbname)
+        {
+
+        }
     }
 }
