dcfcd35a305986661cda4411395793a696788ff9
XavyPonce <jpg79@alu.ua.es>
2017-04-12 19:20:39 +0200

    añado los EN y CAD de usuario

---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/EN/UsuarioEn.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.EN{
+
+    class UsuarioEn{
+
+        // parte privada
+
+        private string usuario;
+        private string direccion;
+        private string contraseña;
+        private string ciudad;
+        private string pais;
+        private string descripcion;
+        private string email;
+        private int edad;
+
+        //********************************************
+        // constructores
+
+        public UsuarioEn() {
+            usuario = "";
+            direccion = "";
+            contraseña = "";
+            ciudad = "";
+            pais = "";
+            descripcion = "";
+            email = "";
+            edad = 0;
+        }
+        /*
+        public UsuarioEn() {
+            usuario = "";
+            direccion = "";
+            contraseña = "";
+            ciudad = "";
+            pais = "";
+            descripcion = "";
+            email = "";
+            edad = 0;
+        }
+        */
+
+        //********************************************
+        // get set
+
+        private string Usuario {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        private string Direccion {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        private string Contraseña {
+            get { return contraseña; }
+            set { contraseña = value; }
+        }
+
+        private string Ciudad {
+            get { return ciudad; }
+            set { ciudad = value; }
+        }
+
+        private string Pais {
+            get { return pais; }
+            set { pais = value; }
+        }
+
+        private string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        private string Email {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private int Edad {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        //********************************************
+        // metodos
+    }
+}
