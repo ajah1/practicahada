12368632b395cf8fd27000f709d1bc1041738655
Alfonso Aracil Andrés <alfonso.aracil.andres@gmail.com>
2017-04-13 16:19:41 +0200

    CAD y EN añadidos, métodos vacíos

---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/CAD/caddatosbancarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.CAD
+{
+    public class CADdatosbancarios
+    {
+
+        public CADdatosbancarios()
+        {
+            
+        }
+
+        public void create()
+        {
+
+        }
+
+        public void add()
+        {
+
+        }
+
+        public void remove()
+        {
+
+        }
+    }
+}
---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/EN/endatosbancarios.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.EN
+{
+    public class DatosBancarios
+    {
+
+        private string titular;
+        private ulong numTarjeta;
+        private DateTime fechaVencimiento;
+        private ushort cvc;
+        // private Usuario usuario;
+
+        public DatosBancarios()
+        {
+            titular = "";
+            numTarjeta = 0;
+            fechaVencimiento = new DateTime(1, 1, 1);
+            cvc = 0;
+        }
+
+        public DatosBancarios(string titular, ulong numTarjeta, DateTime fechaVencimiento, ushort cvc)
+        //DatosBancarios datosbancarios
+        {
+            this.titular = titular;
+            this.numTarjeta = numTarjeta;
+            this.fechaVencimiento = fechaVencimiento;
+            this.cvc = cvc;
+            //this.usuario = usuario;
+        }
+
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+   
+        public ulong NumTarjeta
+        {
+            get { return numTarjeta; }
+            set { numTarjeta = value; }
+        }
+
+        public DateTime FechaVencimiento
+        {
+            get { return fechaVencimiento; }
+            set { fechaVencimiento = value; }
+        }
+
+        public ushort CVC
+        {
+            get { return cvc; }
+            set { cvc = value; }
+        }
+
+
+        public void saveDatosBancarios(string dbname)
+        {
+
+        }
+
+        public void addDatosBancarios(string dbname)
+        {
+
+        }
+
+        public void removeDatosBancarios(string dbname)
+        {
+
+        }
+    }
+}
