76d7ab27e0718d209674b8424b5f0a7034c21d37
aliagaHyder <ajah1@alu.ua.es>
2017-04-28 12:26:44 +0200

    Proyecto para pruebas del en/cad
    ademas de cosa alguna cosa que he cambiado

---
+++ BASE DE DATOS/practicahadagrupal/pruebasENCAD/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasENCAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PARA EJECUTAR:
+            // poner este proyecto como principal
+        }
+    }
+}
