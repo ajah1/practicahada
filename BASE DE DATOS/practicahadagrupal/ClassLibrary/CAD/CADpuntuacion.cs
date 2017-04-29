3fd39f99c269d02f3b36cb9f86559b7ed6e125bc
jose <jigm11@alu.ua.es>
2017-04-29 10:10:37 +0200

    arreglando conflictos

---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/CAD/CADpuntuacion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace ClassLibrary.CAD{
+	public class CADpuntuacion{
+		public CADpuntuacion() { }
+
+		public void add(EN.puntuacion p){
+
+		}
+
+		public void remove(string usuario){
+
+		}
+
+		public void read(string usuario){
+
+		}
+
+		public void update(EN.puntuacion p){
+
+		}
+	}
+}
