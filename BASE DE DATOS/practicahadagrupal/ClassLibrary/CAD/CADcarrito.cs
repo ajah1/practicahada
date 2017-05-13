ac5f83c37d5d1706aef7b04e7a3c816650214398
mariopuche <mpa64@alu.ua.es>
2017-05-13 16:12:22 +0200

    Añadiendo CAD sin acabar de carrito y lineapedido

---
+++ BASE DE DATOS/practicahadagrupal/ClassLibrary/CAD/CADcarrito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace ClassLibrary.CAD
+{
+	public class CADcarrito
+	{
+		const string s = "data source=.\\SQLEXPRESS;Integrated"
+				   + "Security = SSPI; AttachDBFilename =| DataDirectory |\\Database1.mdf;"
+				   + "User Instance = true";
+		
+		SqlConnection conn = new SqlConnection(s);
+
+		public CADcarrito()
+		{
+		}
+
+	}
+}
